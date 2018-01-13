2464189cd3203cc5f4131e1d40162ed1a5df0c88
phanvancong1996 <phanvancong7@gmail.com>
2018-01-13 21:09:19 +0700

    them

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('view engine','ejs');
 app.set('views','./views');
 app.use(express.static('public'));
 
+app.listen(process.env.PORT || 3000,() => console.log('Server started'));
+
 app.get('/',(req, res) => res.render('home'));
 
-app.listen(3000,() => console.log('Server started'));
